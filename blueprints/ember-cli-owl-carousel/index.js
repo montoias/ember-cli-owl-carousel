5f29861d64b3652e6e81482d671f70921cccd482
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2017-06-01 19:10:16 +0100

    Override normalize entity name

--- blueprints/ember-cli-owl-carousel/index.js
+++ blueprints/ember-cli-owl-carousel/index.js
@@ -3,6 +3,8 @@
 module.exports = {
   description: 'add owl.carousel bower package',
 
+  normalizeEntityName() {},
+
   afterInstall() {
     return this.addBowerPackageToProject('owl.carousel', '~2.2.1');
   }
